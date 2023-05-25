0384bc3d0c3483b5cf4a44584196636a11abacd6
elenejavakhishvili638 <elenejavaxishvili@yahoo.com>
2023-05-25 05:38:28 -0700

    feat: add validation

--- app/Http/Controllers/Registration/RegistrationController.php
+++ app/Http/Controllers/Registration/RegistrationController.php
@@ -2,16 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreUserRequest;
 use App\Models\User;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
 class RegistrationController extends Controller
 {
-    public function store()
+    public function store(StoreUserRequest $request): JsonResponse
     {
+        $attributes = $request->validated();
 
-        $user = User::create();
+        $password = $attributes['password'];
+
+        $attributes['password'] = Hash::make($password);
+
+        $user = User::create($attributes);
 
         auth()->login($user);
 
