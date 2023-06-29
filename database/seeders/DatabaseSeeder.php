f95a27dfeb7cf942485efda400bd2c0a58d67818
elenejavakhishvili638 <elenejavaxishvili@yahoo.com>
2023-06-29 13:53:49 -0700

    refactor: swagger files

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,9 +4,6 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Genre;
-use App\Models\Movie;
-use App\Models\Quote;
-use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -16,10 +13,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(1)->create();
-        // Movie::factory(5)->create();
-        // Quote::factory(10)->create();
-
         $genres = ['Action', 'Animation', 'Adventure', 'Biography', 'Comedy', 'Crime', 'Drama', 'Documentary', 'Fantasy', 'Historical', 'Horror', 'Melodrama', 'Musical', 'Reality', 'Science', 'Thriller', 'Western'];
 
         foreach ($genres as $genre) {
